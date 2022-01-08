e2df93efe5b26e6fbd1e8da21c4c637f58746027
323777862@mby.co.il <323777862@mby.co.il>
2022-01-08 22:05:50 +0200

    getprice

--- BL/CostumerProductBl.cs
+++ BL/CostumerProductBl.cs
@@ -23,10 +23,10 @@ namespace BL
         }
         public async Task<Costumerproduct> post(Costumerproduct costumerproduct)
         {
-           
-       
+
+            string link = costumerproduct.Productlink;
             WebRequest request;
-            request = WebRequest.Create("http://127.0.0.1:9007/getprice/?url=" + "https://www.terminalx.com/babyboys/bodysuits-overalls/overalls/z268730004?color=6");
+            request = WebRequest.Create("http://127.0.0.1:9007/getprice/?url=" + link);
             WebResponse response = request.GetResponse();
             string responseFromServer = string.Empty;
             using (Stream dataStream = response.GetResponseStream())
@@ -37,12 +37,55 @@ namespace BL
                 responseFromServer = reader.ReadToEnd();
                 // Display the content.
                 Console.WriteLine(responseFromServer);
+
+              
             }
             //טיפול בתשובה שחזרה מהשרת
             response.Close();
-       
+            int start=responseFromServer.IndexOf("[");
+            int end = responseFromServer.IndexOf("]");
+            string priceres=responseFromServer.Substring(start + 1,end-start-1);
+            Console.WriteLine(priceres);
+            string []pricearr = priceres.Split(",");
+            string baseprice;
+            costumerproduct.Baseprice = 8;
+
+            if (pricearr.Length == 3)
+                baseprice = pricearr[1];
+            else if (pricearr.Length == 2)
+                baseprice = pricearr[0];
+            else if (pricearr.Length == 4)
+                baseprice = pricearr[3];
+            else
+                baseprice = pricearr[0];
+            baseprice = baseprice.Trim();
+          // int pointon=ba
+             float pricef=float.Parse(baseprice);
+            int price = (int)(pricef);
+          costumerproduct.Baseprice = price;
+            Console.WriteLine(price);
+
+
+            WebRequest request2;
+            request2 = WebRequest.Create("http://127.0.0.1:9007/getDesc/?url=" + link);
+            WebResponse response2 = request2.GetResponse();
+            string responseFromServer2 = string.Empty;
+            using (Stream dataStream2 = response2.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader2 = new StreamReader(dataStream2);
+                // Read the content.
+                responseFromServer2 = reader2.ReadToEnd();
+                // Display the content.
+                Console.WriteLine(responseFromServer2);
+
+
+            }
+            //טיפול בתשובה שחזרה מהשרת
+            response2.Close();
 
             return await costumerProductdl.post(costumerproduct);
+
         }
         public void delete(int id)
         {
