f328ee13470d23eb0f9b0477aa60a2a8b64f110c
Racheli Erlanger <212628143@PC5-14>
2022-01-24 12:22:04 +0200

    angular

---
+++ priceTracker/src/models/classcompany.ts
@@ -0,0 +1,12 @@
+
+
+export class Company{
+  id:number ;
+  name:string;
+  password:string;
+  companylink:string;
+  packageid?:number;
+  startofsubscription?:Date;
+  constructor()
+  {}
+}
---
+++ priceTracker/src/models/classcostumer.ts
@@ -0,0 +1,8 @@
+export class Costumer{
+    id:number ;
+    email:string;
+    password:string;
+ 
+    constructor()
+    {}
+  }
